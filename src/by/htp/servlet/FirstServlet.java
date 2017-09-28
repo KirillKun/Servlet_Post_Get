d646039e33fd2b50fd8c7733473a3e4886d2283d
KirillKun <kmkunin@gmail.com>
2017-09-28 23:09:16 +0300

    servlet and simple jsp

---
+++ src/by/htp/servlet/FirstServlet.java
@@ -0,0 +1,34 @@
+package by.htp.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+   
+	public FirstServlet() {
+        super();
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+											throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.getWriter().println("This is "+this.getClass().getName()+",using the GET method");
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+											throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		response.getWriter().println("This is "+this.getClass().getName()+",using the POST method");
+	}
+public void destroy(){
+	super.destroy();
+}
+}
